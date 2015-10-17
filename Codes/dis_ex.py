dd085654e2a6ac5d46398aa1f4a714953c196295
Angel Hernandez <ahernandez91@berkeley.edu>
2015-10-17 15:23:32 -0700

    Numerical Analysis Codes added

--- Codes/dis_ex.py
+++
@@ -1,8 +0,0 @@
-def square(x):
-    return x*x
-
-def and_add(f,n):
-    def add_n(k):
-        return f(k) + n
-    return add_n
-
--- Codes/ex1.py
+++
@@ -1,5 +0,0 @@
-def count_stairs(n):
-    if n <= 1:
-        return 1
-    else:
-        return count_stairs(n-1) + count_stairs(n-2)
